a201c82d9114834b42dcab31d5cc8cffe1e1e298
justxiaomo <justxiamo0034@163.com>
2014-12-30 08:00:34 +0800

    f
    f

--- script/restart_game.lua
+++ script/restart_game.lua
@@ -1,8 +1,6 @@
-_MAX_Z_ORDER = 1
+
 function restart_game(args)
-     _ROOT = args['scope']
-     local bs_answer = args['b_answer']
-     local dd=bs_answer:visible(true)
-     _ROOT:runAction(dd)
      
+      args['scope']:setVisible(false)
+      
 end
